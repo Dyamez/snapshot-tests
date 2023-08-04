5b50774125f886aff7cbefb95d298a2c02ecd3fa
Dyamez <dyamez@outlook.com>
2023-08-04 00:53:12 -0700

    rr8 edited

--- src/GitHubCard.js
+++ src/GitHubCard.js
@@ -6,6 +6,20 @@ export default function GitHubCard() {
       <h1>GitHubCard Snapshot Challenge</h1>
       <p>Github User Name: Dyamez</p>
       <img src="./zenitsu.jpg" alt="pic">Image</img>
+      <h2>About Me:</h2>
+        <ul>
+            <li>
+                First Generation American
+            </li>
+            <li>
+                Graduated Bachelor in Arts in 2001
+            </li>
+            <li>
+                Fulfilling a desire to enter the field of Computer 
+                Technology from twenty five years ago.
+            </li>
+        </ul>
+   
     </div>
   );
 }
